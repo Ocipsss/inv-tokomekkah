becb8c3405c518d77ce7bf04a0b93d62356169f8
Septian Pratama <pratacips@gmail.com>
2026-02-09 13:28:45 +0700

    back to turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,14 @@
 import type { NextConfig } from "next";
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
-
 const nextConfig: NextConfig = {
-  // Matikan Turbopack secara total
-  // @ts-ignore
-  turbopack: {},
-  
-  // Solusi untuk WorkerError saat build PWA
-  experimental: {
-    webpackBuildWorker: true,
+  /* @ts-ignore */ 
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-
-  // Pengaturan tambahan untuk menstabilkan prerendering
-  trailingSlash: true,
-  reactStrictMode: false, // Matikan sementara untuk mempercepat build
-
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  /* @ts-ignore */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
